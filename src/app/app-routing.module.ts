9300bdb0f67a6fc943042abf38134514a1fc3e19
Kamil Brzyski <brzyskikamil@gmail.com>
2020-09-12 13:59:33 +0200

    Task3 done, one of many possibilities

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Task3Component } from '../app/task3/task3.component';
 import { Task5Component } from '../app/task5/task5.component';
 
 const routes: Routes = [
+  { path: 'task3', component: Task3Component },
   { path: 'task5', component: Task5Component }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Task5Component } from './task5/task5.component';
 import { Task5ChildComponent } from './task5/task5-child/task5-child.component';
+import { Task3Component } from './task3/task3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Task5Component,
-    Task5ChildComponent
+    Task5ChildComponent,
+    Task3Component
   ],
   imports: [
     BrowserModule,
